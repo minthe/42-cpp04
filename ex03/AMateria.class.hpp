a664e70ddbc79d8b7e1b5b92bb2af333a59744bd
vfuhlenb <minh.tee@gmail.com>
2023-04-27 14:10:49 +0200

    .

--- ex03/AMateria.class.hpp
+++ ex03/AMateria.class.hpp
@@ -1,12 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   AMateria.class.cpp                                 :+:      :+:    :+:   */
+/*   AMateria.class.hpp                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vfuhlenb <vfuhlenb@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/21 16:34:01 by vfuhlenb          #+#    #+#             */
-/*   Updated: 2023/04/24 12:24:17 by vfuhlenb         ###   ########.fr       */
+/*   Created: 2023/04/24 12:24:31 by vfuhlenb          #+#    #+#             */
+/*   Updated: 2023/04/24 12:29:29 by vfuhlenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
+#ifndef AMATERIA_CLASS_H
+# define AMATERIA_CLASS_H
+
+#include <iostream>
+
+class AMateria
+{
+	public:
+
+		AMateria();
+		
+}
+
+#endif
