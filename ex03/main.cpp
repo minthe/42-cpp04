11518b6043430b60af8b83d4103850a82c0da9fb
vfuhlenb <vfuhlenb@students.42wolfsburg.de>
2023-04-27 18:32:24 +0200

    .

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,34 @@
 /*   By: vfuhlenb <vfuhlenb@students.42wolfsburg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/27 17:31:54 by vfuhlenb          #+#    #+#             */
-/*   Updated: 2023/04/27 17:31:55 by vfuhlenb         ###   ########.fr       */
+/*   Updated: 2023/04/27 18:32:06 by vfuhlenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
+int main()
+{
+	IMateriaSource* src = new MateriaSource();
+	src->learnMateria(new Ice());
+	src->learnMateria(new Cure());
+
+	ICharacter* me = new Character("me");
+
+	AMateria* tmp;
+	tmp = src->createMateria("ice"); 
+	me->equip(tmp);
+	tmp = src->createMateria("cure");
+	me->equip(tmp);
+
+	ICharacter* bob = new Character("bob");
+
+	me->use(0, *bob);
+	me->use(1, *bob);
+
+	delete bob;
+	delete me;
+	delete src;
+
+	return 0;
+}
